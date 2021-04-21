ff1d754b454483a539e39032071b5f62dcfc4f60
Jody <zhujieshan@126.com>
2021-04-21 16:02:03 +0800

    first commit

---
+++ testBGM/mars.framework/Headers/comm/verinfo.h
@@ -0,0 +1,11 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION ""
+#define MARS_PATH ""
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2021-03-10 10:07:42"
+#define MARS_TAG ""
+
+#endif
